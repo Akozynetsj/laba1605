afb3302647b63b918f072b42431fe79844be9a96
student <Kozin_dn78@student.itstep.org>
2023-05-16 19:18:10 +0300

    example 1

--- main.py
+++ main.py
@@ -5,3 +5,11 @@ try:
 except:
     print('no problems')
 print('any code////')
+
+def checker(var):
+   if type(var)!= str:
+       raise TypeError(f'Ssoorryyy,we cant work with{type(var)}, nam treba str')
+   else:
+    return var
+a = 1234
+checker(a)
