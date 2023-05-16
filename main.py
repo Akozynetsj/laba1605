c0360916a76cebd0c1d09ccd91e6927a59cdb523
student <Kozin_dn78@student.itstep.org>
2023-05-16 20:40:50 +0300

    3 завдання

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
 #3
+class InvalidFileFormatError(Exception):
+    def __int__(self, f):
+        self.f = f
+    def read_file(f):
+        try:
+            with open(f, 'r') as file:
+                content = file.read()
+                print('Вміст файлу :', content)
+        except IOError:
+            raise InvalidFileFormatError(f)
+
+try:
+    read_file(input('введіть назіу файлу:'))
+except InvalidFileFormatError as e:
+    print(f"невірний формат файлу {e.f}"
+          f"підтримують тільки текстові файли")
