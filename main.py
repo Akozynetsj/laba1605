2587d1f94a69bcf9e7930d655a89b66b6f64aabe
student <Kozin_dn78@student.itstep.org>
2023-05-16 20:30:28 +0300

    ex 3

--- main.py
+++ main.py
@@ -1,15 +1,1 @@
-#2
-class InvalidPasswordError(Exception):
-    def __int__(self, password):
-        self.password = password
-def validate_password(password):
-         if len(password) < 8:
-             raise InvalidPasswordError(password)
-         else:
-             print('пароль задано')
-try:
-    password = input('введіть пароль')
-    validate_password(password)
-except InvalidPasswordError as e:
-             print(f"пароль введено не коректно {e.password} "
-                   f"мінімум 8 символів")
+#3
